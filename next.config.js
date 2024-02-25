94723ad3dfbef06cc3f6f4e3ad418488faffbf52
Khaled Mostafa <84389471+khaled-farahat@users.noreply.github.com>
2024-02-25 20:27:09 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/bloggy",
+  output: "exports",
   experimental:{
     serverComponentsExternalPackages: ["mongoose"],
   },
